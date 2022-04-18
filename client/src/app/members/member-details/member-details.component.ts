65ddf55a6089963ca0aa23dd3d7445cf9804b63a
anasAsmar99 <anasbelal@hotmail.com>
2022-04-18 16:47:56 +0300

    End of section 10

--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
-import { url } from 'inspector';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
